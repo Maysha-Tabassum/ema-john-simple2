906930491bf37ab8fae9ddffed1202ea910be0ba
Maysha-Tabassum <mayshatabassum77@gmail.com>
2023-09-22 15:54:00 -0700

    show data from cart

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -17,10 +17,39 @@ const Shop = () => {
 
     useEffect(() => {
         const storedCart = getShoppingCart();
-        console.log(storedCart);
-    }, [])
+        const savedCart = [];
+        //step 1: get id of the added product
+        for (const id in storedCart) {
+            //step 2: get product from state by using id
+            const addedProduct = products.find(product => product.id === id)
+            if (addedProduct) {
+                //step 3: add quantity
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                //step 4: add the added product to the savedcart
+                savedCart.push(addedProduct);
+
+            }
+            // console.log('added product', addedProduct)
+        }
+        //step 5: set the cart
+        setCart(savedCart);
+    }, [products])
+
+
     const handleAddToCart = (product) => {
-        const newCart = [...cart, product];
+        // const newCart = [...cart, product];
+        let newCart = [];
+        const exists = cart.find(pd => pd.id === product.id)
+        if (!exists) {
+            product.quantity = 1;
+            newCart = [...cart, product]
+        }
+        else {
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd => pd.id !== product.id)
+            newCart = [...remaining, exists]
+        }
         setCart(newCart);
         addToDb(product.id)
     }
